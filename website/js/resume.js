dd42e72f805b88158453cd0c54e1a472a05504de
William Hucks <wdhucks@gmail.com>
2023-07-30 21:56:15 -0400

    first commit

---
+++ website/js/resume.js
@@ -0,0 +1,38 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+
+})(jQuery); // End of use strict
+
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://63v3flzf6bfotu42lbm7estdea0tijvh.lambda-url.us-east-2.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
+}
+
+updateCounter();
